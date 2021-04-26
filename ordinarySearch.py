9313ba217496aa7cc89faba2ebc3e3ebd3e639ee
Naughty Galileo <55594771+Naughty-Galileo@users.noreply.github.com>
2021-04-26 14:25:04 +0800

    Add files via upload

--- ordinarySearch.py
+++ ordinarySearch.py
@@ -7,13 +7,13 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-def ordinarySearch(dataBaseInitial2, test2, algorithm, num):
+def ordinarySearch(dataBaseInitial2, test2, algorithm, k):
     dataBaseInitial2 = np.array(dataBaseInitial2)
     dataBase2 = dataBaseInitial2[:, 1:217]
 
-    neigh2 = NearestNeighbors(n_neighbors=num, algorithm=algorithm)  # kd_tree brute ball_tree
+    neigh2 = NearestNeighbors(n_neighbors=k, algorithm=algorithm)  # kd_tree brute ball_tree
     neigh2.fit(dataBase2)
-    dis2, index2 = neigh2.kneighbors(test2, num)
+    dis2, index2 = neigh2.kneighbors(test2, k)
     data2 = np.array([dataBaseInitial2[index2[0][0]]])
     for i in range(1, dis2.shape[1]):
         data2 = np.vstack((data2, [dataBaseInitial2[index2[0][i]]]))
@@ -23,23 +23,33 @@ def ordinarySearch(dataBaseInitial2, test2, algorithm, num):
 
 if __name__ == '__main__':
     # 导入数据
-    dataBaseInitial = np.array(pd.read_csv('data/TEMP_SMOTENC/data.csv'))
-    testBaseInitial = np.array(pd.read_csv('data/TEMP_SMOTENC/test.csv'))
-    dataBase = dataBaseInitial[:, 1:217]
-    testBase = testBaseInitial[:, 1:217]
+    dataBaseInitial = pd.read_csv('data/TEMP_SMOTENC/data_selected.csv')
+    queryBaseInitial = pd.read_csv('data/TEMP_SMOTENC/query_selected.csv')
+
+    # # 确定数据集大小及维度
+    featureNum, dimension = dataBaseInitial.shape
+
+    queryBase = np.array(queryBaseInitial.iloc[:, 1:dimension])
+    dataBase = np.array(dataBaseInitial.iloc[:, 1:dimension])
+
+    # ordinary搜索 线性 kd_tree ball_tree
+    alg = 'brute'
+    print('{}搜索方案：'.format(alg))
+    # for num in range(10, 110, 10):
+    #     print(num)
 
     # 计算运行时间
     begin_time = time()
 
-    # ordinary搜索 线性 kd_tree ball_tree
-    print('ball_tree搜索方案：')
-    neigh = NearestNeighbors(n_neighbors=10, algorithm='ball_tree')  # kd_tree brute ball_tree
+    neigh = NearestNeighbors(n_neighbors=10, algorithm= alg)  # kd_tree brute ball_tree
     neigh.fit(dataBase)
-    for m in range(testBase.shape[0]):
-        test = np.array([testBase[m, :]])
+
+    for m in range(queryBase.shape[0]):
+        test = np.array([queryBase[m, :]])
         dis, index = neigh.kneighbors(test, 10)
         print('test{} '.format(m))
         print(dis)
+        print(index)
 
     end_time = time()
     print('搜索时间：', end_time - begin_time)
